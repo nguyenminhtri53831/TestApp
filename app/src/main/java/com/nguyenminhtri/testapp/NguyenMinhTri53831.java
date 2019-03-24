ed8734eb2a943d833e25e7b2f3ac49e9d80c9713
Minh Trí đẹp trai <nguyenminhtri455@gmail.com>
2019-03-24 20:09:42 +0700

    Initial commit

---
+++ app/src/main/java/com/nguyenminhtri/testapp/NguyenMinhTri53831.java
@@ -0,0 +1,15 @@
+package com.nguyenminhtri.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NguyenMinhTri53831 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //nguyenminhtri53831
+    }
+}
